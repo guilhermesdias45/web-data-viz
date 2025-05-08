e1c72144196eae41f88479b7aa3a92e5511cda18
guilhermesdias45 <guilherme.sdias@sptech.school>
2025-05-08 14:06:09 -0300

    continuação das alterações de cadastro - cadastro.html, usuarioController.js e usuarioModel.js

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -54,18 +54,24 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
+    var lastname = req.body.lastnameServer;
     var email = req.body.emailServer;
     var  cpf = req.body.cpfServer;
+    var phone = req.body.phoneServer;
     var senha = req.body.senhaServer;
     var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (lastname == undefined) {
+        res.status(400).send("Seu sobrenome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (cpf == undefined) {
         res.status(400).send("Seu cpf está undefined!");
+    } else if (phone == undefined) {
+        res.status(400).send("Seu telefone está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else if (fkEmpresa == undefined) {
@@ -73,7 +79,7 @@ function cadastrar(req, res) {
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa, cpf)
+        usuarioModel.cadastrar(nome, lastname, email, cpf, phone, senha, fkEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -10,12 +10,21 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fkEmpresa, cpf) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa, cpf);
-    
+function cadastrar(nome, lastname, email, cpf, phone, senha, fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, lastname, email, cpf, phone, senha, fkEmpresa);
+
+    phone
+    5511912345678
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
+    INSERT INTO representante (nome_representante, cpf_representante, fkEmpresa, senha)
+    VALUES 
+    ('${nome} ${lastname}', '${cpf}', ${fkEmpresa}, '${senha}');
+    INSERT INTO contato_representante (email_representante, codigo_internacional, ddd, telefone, fkRepresentante)
+    VALUES ('${email}', '55', '11', '912345678', (select idRepresentante from representante where nome_representante = '${nome} ${lastname}' and cpf_representante = '${cpf}'));
+
         INSERT INTO usuario (nome, email, senha, fk_empresa, cpf) VALUES ('${nome}', '${email}', '${senha}', ${fkEmpresa}, '${cpf}');`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
